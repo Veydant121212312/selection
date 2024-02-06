4b94715d1c85983c8c0c9cfd984d0aa84ca8b6bb
21VGogna <21VGogna@B105-20>
2024-02-06 09:29:44 +0000

    Add project files.

---
+++ selection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mynum = 547;
+            int mynext = 600;
+            if (mynum > mynext)
+            {
+                Console.WriteLine("yes");
+            }
+            else 
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
